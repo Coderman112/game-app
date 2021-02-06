d3c29c19ba87413eb9e394d37751d02398012d5e
ChristianCudby <christiancudby@hotmail.com>
2021-02-05 22:21:10 -0500

    finishes the create_games method in the Scrapr class

--- lib/game.rb
+++ lib/game.rb
@@ -7,7 +7,9 @@ class Game
         @@all << self
     end
 
-    
+    def self.all
+        @@all
+    end
 
 
 end
--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,7 +4,7 @@ require 'pry'
 
 require_relative './game.rb'
 
-class Scraper
+class Scraper 
 
     def get_page
         Nokogiri::HTML(URI.open("https://rawg.io"))
@@ -12,27 +12,40 @@ class Scraper
     end
 
     def find_games
-        self.get_page.css("game-card-medium")
+        self.get_page.css(".game-card-medium")
     end
 
     def create_games
+        @platforms = []
         self.find_games.each do |game_card_medium|
             game = Game.new
-            game.title = game_card_medium.css("heading heading 4").text
-            game.platforms = game_card_medium.css("platforms platforms_medium game-card-medium_platforms")
-            if game.platforms.include?("platforms__platform platforms__platform_medium platforms__platform_pc")
-                game.platforms = []
-                game.platforms << "PC"
+            game.title = game_card_medium.css(".heading.heading_4").text
+            platform_array = game_card_medium.css("div .platforms")[0].children
+            platform_string = platform_array.to_s
+            #binding.pry
+            if platform_string.include?("platforms__platform platforms__platform_medium platforms__platform_pc")
+                @platforms << "PC"
             end
-            if game.platforms.include?("platforms__platform platforms__platform_medium platforms__platform_playstation")
-                game.platforms << "Playstation"
+            if platform_string.include?("platforms__platform platforms__platform_medium platforms__platform_playstation")
+                @platforms << "Playstation"
             end
-            if game.platforms.include?("platforms__platform platforms__platform_medium platforms__platform_xbox")
-                game.platforms << "Xbox"
+            if platform_string.include?("platforms__platform platforms__platform_medium platforms__platform_xbox")
+                @platforms << "Xbox"
             end
             
         end
     end
-    binding.pry
+    
+    def print_games
+        self.create_games
+        Game.all.each do |game|
+          if game.title && game.title != ""
+            puts "Title: #{game.title}"
+            puts "Platforms: #{@platforms.uniq}"
+          end
+        end
+    end
+
 
 end
+Scraper.new.print_games
