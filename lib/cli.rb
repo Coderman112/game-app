d563d37e15281ec9814b4d62f217dd85be98531f
ChristianCudby <christiancudby@hotmail.com>
2021-02-07 20:26:00 -0500

    removes comments that were still in the program

--- lib/cli.rb
+++ lib/cli.rb
@@ -13,34 +13,6 @@ class Cli < Game
         puts "Go ahead and put in a game title and I'll find its platforms for you"
         input = gets.strip
         Game.find_by_title(input)
-        # if game.title == input
-        #     puts game.platforms
-        # end
-
-        # if game.title != input
-        #     puts "Sorry I couldn't find that fuckin shit bitch"
-        # end
-        # # Game.construct_games
-        #  second_counter = 0
-        #  found_it = 0
-        # while second_counter < $game_data.length do
-        #     if $game_data[second_counter][0] == input
-        #         if $game_data[second_counter][1] != nil
-        #         puts "It's on #{$game_data[second_counter][1]}"
-        #         end
-        #         if $game_data[second_counter][2] != nil 
-        #         puts "It's on #{$game_data[second_counter][2]}"
-        #         end
-        #         if $game_data[second_counter][3] != nil
-        #         puts "It's on #{$game_data[second_counter][3]}"
-        #         end
-        #         found_it += 1
-        #     end
-        #      second_counter += 1
-        # end
-        # if found_it == 0
-        #     puts "Sorry I couldn't find it"
-        # end
         self.search_again_or_quit
     end
 
